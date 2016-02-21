0f1a981f8a627be3b14dff6cbdd2bca2bc356443
krazedkrish <krazedkrish@gmail.com>
2016-02-21 09:26:17 +0545

    adds tray icon

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
 var show_tray_icon = function() {
-    //show icons
+  //show icons
 
-    // Load native UI library
-    var gui = require('nw.gui');
+  // Load native UI library
+  var gui = require('nw.gui');
+  // Create a tray icon
+  var tray = new gui.Tray({ title: 'Tray', icon: 'resources/icon.png' });
 
-    // Create a tray icon
-    var tray = new gui.Tray({ title: 'Tray', icon: 'resources/icon.png' });
 
+  // Bind a callback to item
+  var quitMenuItem = new gui.MenuItem({
+    label: "Quit",
+    click: function() {
+      gui.App.closeAllWindows();
+      gui.App.quit();
+    },
+    key: "x",
+    modifiers: "ctrl-alt",
+  });
 
-    // Bind a callback to item
-    quitMenuItem = new gui.MenuItem({
-        label: "Quit",
-        click: function() {
-            gui.App.closeAllWindows();
-            gui.App.quit();
-        },
-        key: "x",
-        modifiers: "ctrl-alt",
-    });
+  // // Bind a callback to item
+  // var showMenuItem = new gui.MenuItem({
+  //   label: "EmitIt",
+  //   click: function() {
+  //       var win = gui.Window.get();
+  //   },
+  //   key: "E",
+  //   modifiers: "ctrl-alt",
+  // });
 
-    // Give it a menu
-    var menu = new gui.Menu();
-    menu.append(quitMenuItem);
-    tray.menu = menu;
+  // Give it a menu
+  var menu = new gui.Menu();
+  // menu.append(showMenuItem);
+  menu.append(quitMenuItem);
+  tray.menu = menu;
 
 }
 
+//var gui = require('nw.gui');
 show_tray_icon();
-
